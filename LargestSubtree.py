7dd5a195547bdd8ed19d35cfeaf12211bf28c661
Kevin Tran <55123553+kevct@users.noreply.github.com>
2020-09-16 16:21:11 -0700

    Create LargestSubtree.py

---
+++ LargestSubtree.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 16 16:04:42 2020
+
+@author: kevin
+"""
+
+"""
+This problem was recently asked by Twitter:
+You are given the root of a binary tree. Find and return the largest subtree 
+of that tree, which is a valid binary search tree.
+Here's a starting point:
+"""
+class TreeNode:
+  def __init__(self, key):
+    self.left = None
+    self.right = None
+    self.key = key
+
+  def __str__(self):
+    # preorder traversal
+    answer = str(self.key)
+    if self.left:
+      answer += str(self.left)
+    if self.right:
+      answer += str(self.right)
+    return answer
+
+def size(root):
+    if(not root):
+        return 0
+    return 1 + size(root.left) + size(root.right)
+
+def largest_bst_subtree(root):
+  # Fill this in.
+  rightSize = size(root.right)
+  leftSize = size(root.left)
+  if(rightSize > leftSize):
+      return str(root.right)
+  else:
+      return str(root.left)
+  
+
+#     5
+#    / \
+#   6   7
+#  /   / \
+# 2   4   9
+node = TreeNode(5)
+node.left = TreeNode(6)
+node.right = TreeNode(7)
+node.left.left = TreeNode(2)
+node.right.left = TreeNode(4)
+node.right.right = TreeNode(9)
+print(largest_bst_subtree(node))
+#749
