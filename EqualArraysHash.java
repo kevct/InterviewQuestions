48e7161b52b2ec99bc2b1fa2da2e046b80d34f13
kevct <55123553+kevct@users.noreply.github.com>
2020-12-23 13:32:25 -0800

    Create EqualArraysHash.java:

--- EqualArraysHash.java
+++ EqualArraysHash.java
@@ -4,9 +4,10 @@ Two arrays are said to be equal if both of them contain same set of elements, ar
 of elements may be different though.
 Note : If there are repetitions, then counts of repeated elements must also be same for two array to be equal.
 */
-import java.util.Arrays;
 
-class EqualArrays{
+import java.util.HashMap;
+
+class EqualArraysHash{
    public static void main(String[] args){
       int arr1[] = {0, 1, 2, 3, 4};
       int arr2[] = {4, 3, 2, 1, 0};
@@ -21,16 +22,19 @@ class EqualArrays{
    * @return true if arrays are equal, false otherwise
    */
    static boolean isEqual(int arr1[], int arr2[]){
+      HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
       if(arr1.length != arr2.length){
          return false;
       }
-      Arrays.sort(arr1);
-      Arrays.sort(arr2);
+      for(Integer i : arr1){
+         hm.put(i, hm.getOrDefault(i, 0) + 1);
+      }
       
-      for(int i = 0; i < arr1.length; i++){
-         if(arr1[i] != arr2[i]){
+      for(Integer i : arr2){
+         if(hm.get(i) == null || hm.get(i) == 0){
             return false;
          }
+         hm.put(i, hm.getOrDefault(i, 0) - 1);
       }
       return true;
    }
